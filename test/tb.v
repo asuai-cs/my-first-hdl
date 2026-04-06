adb50ae7ed27721b0d064aded4bcec92c0eb0078
Chukwunalu (J) Asuai <jewelsh1991@gmail.com>
2026-04-06 18:27:14 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
- module tt_um_asuai_cs_my_first_hdl (
+ tt_um_asuai_cs_my_first_hdl dut (
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
